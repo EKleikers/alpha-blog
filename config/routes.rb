ed1464773c0c76d602a6bbecdc108e9d197ca80b
esther-kleikers <esther.kleikers@aisler.team>
2026-07-19 12:23:22 +0200

    aricles/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  # get "up" => "rails/health#show", as: :rails_health_check
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
   root 'pages#home'
   get 'about', to:'pages#about'
+  resources :articles, only: [:show]
 
 end
+
+
+# get "up" => "rails/health#show", as: :rails_health_check
+# get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+# get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
