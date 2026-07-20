7f943de83952e720020e778ee53f4f65c78741c9
esther-kleikers <esther.kleikers@aisler.team>
2026-07-20 12:14:41 +0200

    articles: action links & dry

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,11 +12,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
   def create
     # render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article
     if @article.save
       flash[:notice]="Article is create successfully"
@@ -27,12 +26,26 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice]="Article is create successfully"
+    if @article.update(article_params)
+      flash[:notice]="Article is created successfully"
       redirect_to @article
     else
       render 'edit'
     end
   end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
