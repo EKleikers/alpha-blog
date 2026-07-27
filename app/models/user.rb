3140d78596a330e8a5a83589669d00e7bbd4520f
esther-kleikers <esther.kleikers@aisler.team>
2026-07-27 16:23:47 +0200

    user create action added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase}
   has_many :articles
   validates :username,
                       uniqueness: { case_sensitive: false },
@@ -10,4 +11,5 @@ class User < ApplicationRecord
                       presence: true,
                       length: { maximum: 105 },
                       format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
