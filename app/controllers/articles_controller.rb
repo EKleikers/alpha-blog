243993b6fe562bfa59e106574e7dcc2a9b0bc9ff
esther-kleikers <esther.kleikers@aisler.team>
2026-08-05 11:19:51 +0200

    final_many-to-many

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -16,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article]
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     # render plain: @article
     if @article.save
       flash[:notice]="Article is create successfully"
@@ -46,7 +48,15 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
+
+  def require_same_user
+    if current_user !=@article.user && !current_user.admin?
+      flash[:alert]="You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
+
 
 end
